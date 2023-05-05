53bdd785be36ecf391c99f2d92723b0f7df27dad
kanosawa <kanokanosawasawa@gmail.com>
2023-05-06 02:07:19 +0900

    Chapter06のファイル構成を変更

--- Chapter06/Chapter06/main.cpp
+++ Chapter06/Chapter06/main.cpp
@@ -43,15 +43,15 @@ void main() {
 
 	// Chapter04
 	auto vertexBuffer = createVertexBuffer(dev, UINT64(sizeof(vertices[0])) * vertices.size());
-	mapVertexBuffer(vertexBuffer, vertices);
-	auto vertexBufferView = createVertexBufferView(vertexBuffer, vertices);
+	mapVertexBuffer05(vertexBuffer, vertices);
+	auto vertexBufferView = createVertexBufferView05(vertexBuffer, vertices);
 	auto indexBuffer = createIndexBuffer(dev, UINT64(sizeof(indices[0])) * indices.size());
 	mapIndexBuffer(indexBuffer, indices);
 	auto indexBufferView = createIndexBufferView(indexBuffer, indices);
 	auto vertexShaderBlob = createVertexShaderBlob();
 	auto pixelShaderBlob = createPixelShaderBlob();
-	auto rootSignature = createRootSignature(dev);
-	auto inputLayout = createInputLayout();
+	auto rootSignature = createRootSignature06(dev);
+	auto inputLayout = createInputLayout05();
 	auto pipelineState = createGraphicsPipelineState(dev, vertexShaderBlob, pixelShaderBlob, rootSignature, inputLayout);
 	auto viewport = createViewPort(windowWidth, windowHeight);
 	auto scissorRect = createScissorRect(windowWidth, windowHeight);
@@ -62,7 +62,7 @@ void main() {
 	createShaderResourceView(dev, texBuffer, basicDescriptorHeap, 0);
 
 	// 定数バッファ
-	auto constBuffer = createConstBuffer(dev);
+	auto constBuffer = createConstBuffer06(dev);
 	createConstantBufferView(dev, constBuffer, basicDescriptorHeap, 1);
 	
 	// マトリクス作成
@@ -92,7 +92,7 @@ void main() {
 		auto worldMat = XMMatrixRotationY(angle);
 		*mapMatrix = worldMat * viewMat * projMat;
 
-		render(dev, rtvDescriptorHeap, commandList, vertexBufferView, indexBufferView, swapChain, rootSignature, pipelineState, viewport, scissorRect, basicDescriptorHeap);
+		render05(dev, rtvDescriptorHeap, commandList, vertexBufferView, indexBufferView, swapChain, rootSignature, pipelineState, viewport, scissorRect, basicDescriptorHeap);
 		commandList->Close();
 
 		ID3D12CommandList* constCommandList[] = { commandList };
